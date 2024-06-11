650e321882d3225a698abd9fd55213c2c8853dc4
JFolberth <johnfolberth@gmail.com>
2024-06-11 19:16:53 -0400

    Update bundle-deploy.sh

--- bundle_adb_360/helpers/bundle-deploy.sh
+++ bundle_adb_360/helpers/bundle-deploy.sh
@@ -26,6 +26,7 @@ export ARM_TENANT_ID=$tenantid
 # this is going to add ths sp to the workspace
 export DATABRICKS_AZURE_RESOURCE_ID=$workspaceId
 
-cd ../bundle
+# cd ../bundle
+cd bundle_adb_360
 
 databricks bundle deploy -t $env
